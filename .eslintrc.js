828ceab4c7f1297054dfbc65be5a7dcbd394fec0
fiahfy <d.fiahfy@gmail.com>
2018-10-03 16:29:20 +0900

    Use prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
   env: {
-    "jest": true
+    node: true,
+    jest: true
   },
   extends: [
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
-  // add your custom rules here
-  rules: {}
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ]
 }
